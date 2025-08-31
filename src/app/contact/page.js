d1f19643a2311362a4e6b6ea610387a7dcc860c6
Shubham Goyal <111879491+gshubham05@users.noreply.github.com>
2025-08-31 19:40:49 +0530

    map

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -85,12 +85,19 @@ export default function ContactPage() {
 
       {/* Google Map Embed */}
       <div className="max-w-5xl mx-auto">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3443.8720988585355!2d78.05093397556455!3d30.32615567478305!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzDCsDE5JzM0LjIiTiA3OMKwMDMnMTIuNiJF!5e0!3m2!1sen!2sin!4v1756649209102!5m2!1sen!2sin" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
-            referrerPolicy="no-referrer-when-downgrade"
-          className="rounded-xl shadow-lg"
-          aria-label="Google Map showing Fork and Sticks"
-        ></iframe>
-      </div>
+  <iframe
+    src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3443.8720988585355!2d78.05093397556455!3d30.32615567478305!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzDCsDE5JzM0LjIiTiA3OMKwMDMnMTIuNiJF!5e0!3m2!1sen!2sin!4v1756649209102!5m2!1sen!2sin"
+    width="400"
+    height="300"
+    style={{ border: 0 }}
+    allowFullScreen=""
+    loading="lazy"
+    referrerPolicy="no-referrer-when-downgrade"
+    className="rounded-xl shadow-lg w-full"
+    aria-label="Google Map showing Fork and Sticks"
+  ></iframe>
+</div>
+
     </div>
   );
 }
